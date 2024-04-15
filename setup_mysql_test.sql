10b725e811355878b8bc646efa2505fe3a0cdc71
reham-elsyed <rehamshepl666@gmail.com>
2024-04-15 14:25:09 -0700

    p

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,3 +1,8 @@
+-- Creates a MySQL server with:
+--   Database hbnb_test_db.
+--   User hbnb_test with password hbnb_test_pwd in localhost.
+--   Grants all privileges for hbnb_test on hbnb_test_db.
+
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
