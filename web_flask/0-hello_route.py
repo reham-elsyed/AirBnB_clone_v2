15ace60427fe2861d8d7620b3966a2e0eaf11e23
reham elsayed shipl <139706852+reham-elsyed@users.noreply.github.com>
2024-05-17 14:58:31 +0300

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """
-Module to use flask to start application
+starts a Flask application
 """
-
 
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Flask hello world"""
-    return "Hello HBNB!"
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
