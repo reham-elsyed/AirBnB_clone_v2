b831b96df567a3e4dcf52dd386218d319b3e1c7e
reham-elsyed <rehamshepl666@gmail.com>
2024-05-17 05:48:46 -0700

    p

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,20 +1,22 @@
-#/usr/bin/python3
-"""Module to use flask to make application"""
-
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
 
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Hello world"""
-    return "Hello HBNB!"
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """display hbnb for /hbnb"""
-    return "HBNB"
+    """returns HBNB"""
+    return 'HBNB'
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
