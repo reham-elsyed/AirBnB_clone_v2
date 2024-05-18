019446d7315d6c868c314bb00d0eaf2f72c61eac
reham-elsyed <rehamshepl666@gmail.com>
2024-05-18 03:05:36 -0700

    flask

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""Method to create routes"""
+"""
+starts a Flask web application
+"""
 
 from flask import Flask, render_template
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def index():
@@ -34,10 +37,11 @@ def imanumber(n):
     """display “n is a number” only if n is an integer"""
     return "{:d} is a number".format(n)
 
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def numbersandtemplates(n):
-    """display a HTML page only if is inger"""
+    """display a HTML page only if n is an integer"""
     return render_template('5-number.html', n=n)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port='5000')
+    app.run(host='0.0.0.0', port='5000')
